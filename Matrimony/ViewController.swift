a4befd1b4b025c15cb60215fc4b66ddd325ad9e9
Aditya Vyavahare <vvaditya23@icloud.com>
2024-04-20 15:34:17 +0530

    Initial Commit

---
+++ Matrimony/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Matrimony
+//
+//  Created by Aditya Vyavahare on 20/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
